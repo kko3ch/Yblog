1520986a3226a2d0c5562f81a37f226e311b33dc
Ko3ch <tomko3ch@gmail.com>
2020-07-20 18:07:52 +0300

    manage app deployment

--- manage.py
+++ manage.py
@@ -20,7 +20,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, User = User, Post = Post, Comment = Comment)
+    return dict(db = db,app = app, User = User, Post = Post, Comment = Comment)
 
 if __name__ == '__main__':
     manager.run()
